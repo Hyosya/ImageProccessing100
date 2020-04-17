4f394143a0272c56a5ae9e98f86cdd189c9bfcd8
Hyosya <hyosya.hyosya@gmail.com>
2020-04-17 21:22:38 +0900

    回答7の修正

--- ImageProcessing100/ImageProcessing100/Answers/Answer_7.cs
+++ ImageProcessing100/ImageProcessing100/Answers/Answer_7.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Numerics;
+using System.Threading.Tasks;
 
 namespace ImageProcessing100.Answers
 {
@@ -13,9 +15,15 @@ namespace ImageProcessing100.Answers
         {
             var img = Cv2.ImRead("imori.jpg");
 
+            var sw = new Stopwatch();
+            sw.Start();
+
             var output = UsingSubMat(img);
 
-            Cv2.ImWrite("out.jpg", output);
+            sw.Stop();
+            Console.WriteLine(sw.ElapsedTicks);
+
+            //Cv2.ImWrite("out.jpg", output);
             Cv2.ImShow("sample", output);
             Cv2.WaitKey(0);
             Cv2.DestroyAllWindows();
@@ -26,34 +34,34 @@ namespace ImageProcessing100.Answers
             var outMat = Mat.Zeros(img.Rows, img.Height, MatType.CV_8UC3).ToMat();
             const int range = 8;
             const int pixelCount = range * range;
-            var sumBGR = (0d, 0d, 0d);
-            var meanBGR = (0d, 0d, 0d);
             var points = Enumerable.Range(0, img.Rows / range).Select(i => i * range);
             var mainIndexes = points
-                .SelectMany(_ => points, (x, y) => (x, y))
-                .ToArray();
+                .SelectMany(_ => points, (x, y) => (x, y));
+
             var subIndexes = Enumerable.Range(0, range)
                 .SelectMany(_ => Enumerable.Range(0, range), (x, y) => (x, y))
                 .ToArray();
 
             foreach (var (x, y) in mainIndexes)
             {
-                sumBGR = (0d, 0d, 0d);
+                var sumBGR = Vector3.Zero;
                 var outSubIndexer = img.SubMat(y, y + range, x, x + range).GetGenericIndexer<Vec3b>();
                 foreach (var (xi, yi) in subIndexes)
                 {
-                    sumBGR.Item1 += outSubIndexer[xi, yi].Item0;
-                    sumBGR.Item2 += outSubIndexer[xi, yi].Item1;
-                    sumBGR.Item3 += outSubIndexer[xi, yi].Item2;
+                    var pixel = outSubIndexer[xi, yi];
+                    var v = new Vector3(pixel.Item0, pixel.Item1, pixel.Item2);
+                    sumBGR += v;
                 }
-                meanBGR = (sumBGR.Item1 / pixelCount, sumBGR.Item2 / pixelCount, sumBGR.Item3 / pixelCount);
+                var mean = sumBGR / pixelCount;
                 outSubIndexer = outMat.SubMat(y, y + range, x, x + range).GetGenericIndexer<Vec3b>();
                 foreach (var (xi, yi) in subIndexes)
                 {
-                    var pixel = outSubIndexer[xi, yi];
-                    pixel.Item0 = (byte)meanBGR.Item1;
-                    pixel.Item1 += (byte)meanBGR.Item2;
-                    pixel.Item2 += (byte)meanBGR.Item3;
+                    var pixel = new Vec3b
+                    {
+                        Item0 = (byte)mean.X,
+                        Item1 = (byte)mean.Y,
+                        Item2 = (byte)mean.Z
+                    };
                     outSubIndexer[xi, yi] = pixel;
                 }
             }
--- ImageProcessing100/ImageProcessing100/Program.cs
+++ ImageProcessing100/ImageProcessing100/Program.cs
@@ -7,7 +7,7 @@ namespace ImageProcessing100
     {
         static void Main(string[] args)
         {
-            Answer_8.Solve();
+            Answer_7.Solve();
         }
     }
 }
