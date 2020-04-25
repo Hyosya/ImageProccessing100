4fb97dfbc270b43034755643853d62cd60f789f9
Hyosya <hyosya.hyosya@gmail.com>
2020-04-25 18:54:47 +0900

    15問目回答

--- ImageProcessing100/ImageProcessing100/Answers/Answer_015.cs
+++ ImageProcessing100/ImageProcessing100/Answers/Answer_015.cs
@@ -4,13 +4,13 @@ using System.Numerics;
 
 namespace ImageProcessing100.Answers
 {
-    public static class Answer_014
+    public static class Answer_015
     {
         public static void Solve()
         {
             var img = Cv2.ImRead("imori.jpg");
             var gray = Util.GBRToGray(img);
-            var output = MeanFilter(gray, false);
+            var output = PrewittFilter(gray, true);
 
             //Cv2.ImWrite("out.jpg", output);
             Cv2.ImShow("sample", output);
@@ -18,19 +18,20 @@ namespace ImageProcessing100.Answers
             Cv2.DestroyAllWindows();
         }
 
-        private static Mat MeanFilter(Mat img, bool isVertical)
+        private static Mat PrewittFilter(Mat img, bool isVertical)
         {
             var outMat = Mat.Zeros(img.Rows, img.Height, MatType.CV_8UC1).ToMat();
             var kernel = new int[3, 3];
 
-            kernel[1, 1] = 1;
             if (isVertical)
             {
-                kernel[0, 1] = -1;
+                kernel[0, 0] = kernel[0, 1] = kernel[0, 2] = 1;
+                kernel[2, 0] = kernel[2, 1] = kernel[2, 2] = -1;
             }
             else
             {
-                kernel[1, 0] = -1;
+                kernel[0, 0] = kernel[1, 0] = kernel[2, 0] = 1;
+                kernel[0, 2] = kernel[1, 2] = kernel[2, 2] = -1;
             }
 
             var imgIndexer = img.GetGenericIndexer<byte>();
