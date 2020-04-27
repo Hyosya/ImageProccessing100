2d5e788512e784659de4225624894137bccbe9fe
Hyosya <hyosya.hyosya@gmail.com>
2020-04-27 17:40:41 +0900

    17問目回答

---
+++ ImageProcessing100/ImageProcessing100/Answers/Answer_017.cs
@@ -0,0 +1,49 @@
+﻿using OpenCvSharp;
+using System;
+using System.Numerics;
+
+namespace ImageProcessing100.Answers
+{
+    public static class Answer_017
+    {
+        public static void Solve()
+        {
+            var img = Cv2.ImRead("imori.jpg");
+            var gray = Util.GBRToGray(img);
+            var output = LaplacianFilter(gray);
+
+            //Cv2.ImWrite("out.jpg", output);
+            Cv2.ImShow("sample", output);
+            Cv2.WaitKey(0);
+            Cv2.DestroyAllWindows();
+        }
+
+        private static Mat LaplacianFilter(Mat img)
+        {
+            var outMat = Mat.Zeros(img.Rows, img.Height, MatType.CV_8UC1).ToMat();
+            var kernel = new int[3, 3]
+            {
+                    { 0, 1, 0},
+                    { 1, -4, 1},
+                    { 0, 1, 0},
+            };
+
+            var imgIndexer = img.GetGenericIndexer<byte>();
+            var outIndexer = outMat.GetGenericIndexer<byte>();
+            for (int y = 0; y < img.Height; y++)
+                for (int x = 0; x < img.Width; x++)
+                {
+                    var v = 0;
+                    for (int dy = -1; dy < 2; dy++)
+                        for (int dx = -1; dx < 2; dx++)
+                        {
+                            if ((x + dx < 0) || (y + dy < 0) || (x + dx > img.Height) || (y + dy > img.Width)) continue;
+                            var pixel = imgIndexer[y + dy, x + dx];
+                            v += pixel * kernel[dy + 1, dx + 1];
+                        }
+                    outIndexer[y, x] = (byte)Math.Max(Math.Min(v, 255), 0);
+                }
+            return outMat;
+        }
+    }
+}
--- ImageProcessing100/ImageProcessing100/Program.cs
+++ ImageProcessing100/ImageProcessing100/Program.cs
@@ -7,7 +7,7 @@ namespace ImageProcessing100
     {
         static void Main(string[] args)
         {
-            Answer_016.Solve();
+            Answer_017.Solve();
         }
     }
 }
