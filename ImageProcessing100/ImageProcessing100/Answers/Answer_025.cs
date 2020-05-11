e28ae1dd348a0bad69a9a059b4cff4e1a3527853
Hyosya <hyosya.hyosya@gmail.com>
2020-05-11 17:12:28 +0900

    25問目解答

---
+++ ImageProcessing100/ImageProcessing100/Answers/Answer_025.cs
@@ -0,0 +1,40 @@
+﻿using OpenCvSharp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImageProcessing100.Answers
+{
+    public static class Answer_025
+    {
+        public static void Solve()
+        {
+            var img = Cv2.ImRead("imori.jpg");
+
+            var output = NearestNeighbor(img, 1.5, 1.5);
+
+            //Cv2.ImWrite("out.jpg", output);
+            Cv2.ImShow("source", img);
+            Cv2.ImShow("result", output);
+            Cv2.WaitKey(0);
+            Cv2.DestroyAllWindows();
+        }
+
+        private static Mat NearestNeighbor(Mat img, double resizeX, double resizeY)
+        {
+            static int round(in double value) => (int)Math.Round(value, MidpointRounding.AwayFromZero);
+            var outMat = Mat.Zeros(round(img.Rows * resizeX), round(img.Cols * resizeY), img.Type()).ToMat();
+            var imgIndexer = img.GetGenericIndexer<Vec3b>();
+            unsafe
+            {
+                outMat.ForEachAsVec3b((value, position) =>
+                {
+                    var x = round(position[0] / resizeX);
+                    var y = round(position[1] / resizeX);
+                    *value = imgIndexer[x, y];
+                });
+            }
+            return outMat;
+        }
+    }
+}
--- ImageProcessing100/ImageProcessing100/Program.cs
+++ ImageProcessing100/ImageProcessing100/Program.cs
@@ -7,7 +7,7 @@ namespace ImageProcessing100
     {
         static void Main(string[] args)
         {
-            Answer_024.Solve();
+            Answer_025.Solve();
         }
     }
 }
